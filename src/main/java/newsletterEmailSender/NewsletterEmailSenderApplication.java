5dd15ae3fc6c2d78ffaca68d5ebf93176cf5f780
Bartosz Tyra <57729511+fernil@users.noreply.github.com>
2023-04-26 23:41:51 +0200

    Initial commit

---
+++ src/main/java/newsletterEmailSender/NewsletterEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package newsletterEmailSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsletterEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsletterEmailSenderApplication.class, args);
+	}
+
+}
