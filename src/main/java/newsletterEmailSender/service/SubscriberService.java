2b286a7eedc93ed51f41c52c1fb6936b204bf9a3
Bartosz Tyra <57729511+fernil@users.noreply.github.com>
2023-05-03 23:38:04 +0200

    Service annotation added

--- src/main/java/newsletterEmailSender/service/SubscriberService.java
+++ src/main/java/newsletterEmailSender/service/SubscriberService.java
@@ -3,7 +3,7 @@ package newsletterEmailSender.service;
 import newsletterEmailSender.model.Subscriber;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface SubscriberService {
     Subscriber save(Subscriber subscriber);
 }
--- src/main/java/newsletterEmailSender/service/implementation/SubscriberServiceImpl.java
+++ src/main/java/newsletterEmailSender/service/implementation/SubscriberServiceImpl.java
@@ -3,7 +3,9 @@ package newsletterEmailSender.service.implementation;
 import newsletterEmailSender.model.Subscriber;
 import newsletterEmailSender.repository.SubscriberRepository;
 import newsletterEmailSender.service.SubscriberService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SubscriberServiceImpl implements SubscriberService {
     private SubscriberRepository subscriberRepository;
 
